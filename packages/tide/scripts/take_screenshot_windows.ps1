cacd3e7cbc1a28cf13ff2eb1b4d5184e245721a8
Larry Aasen <larryaasen@gmail.com>
2024-12-05 08:59:37 -0500

    CI: updates

--- packages/tide/scripts/take_screenshot_windows.ps1
+++ packages/tide/scripts/take_screenshot_windows.ps1
@@ -1,7 +1,11 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
+Write-Host "Starting screenshot capture..."
+
 $screenshot = [System.Drawing.Bitmap]::new([System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Width, [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Height)
 $graphics = [System.Drawing.Graphics]::FromImage($screenshot)
 $graphics.CopyFromScreen(0, 0, 0, 0, $screenshot.Size)
 $screenshot.Save("screenshot.png", [System.Drawing.Imaging.ImageFormat]::Png)
+
+Write-Host "Screenshot saved to screenshot.png"
